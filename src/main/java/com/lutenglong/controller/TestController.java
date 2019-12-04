bfc3d2a515403a118dedd8607a7260acae730ca6
lutenglong <1093012050@qq.com>
2019-12-04 10:32:57 +0800

    初始化版本

---
+++ src/main/java/com/lutenglong/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.lutenglong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("test")
+	public String test(Model m) {			//跳转页面
+		m.addAttribute("a", "test");
+		return "test";
+	}
+}
